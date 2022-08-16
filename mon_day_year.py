435e9bec5a88e2ba1b1cb78ffe2d3a8d31df52aa
felipmarqs <39883697+felipmarqs@users.noreply.github.com>
2022-08-16 12:07:55 -0300

    Create mon_day_year.py
    A function that receives a date of birth and returns it in a phrase

---
+++ mon_day_year.py
@@ -0,0 +1,5 @@
+def mon_day_year(month:str,day:int,year:int) -> str:
+    '''A function that receives a date of birth and returns it in a phrase'''
+    return print(f'You were born in {month} {day}º of {year}.')
+
+mon_day_year('September',4,2000)
